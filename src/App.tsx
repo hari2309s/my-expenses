ac442ce61b58b69eb93ebc6c625500bfb9bbe9d3
Hariharan Selvaraj <hariharan.2309.s@icloud.com>
2024-11-13 16:08:01 +0100

    style fixes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import AppIcon from './assets/app-icon.png';
 
 const App = () => {
   return (
-    <div className="container m-4 mx-auto my-auto h-screen text-center">
+    <div className="container m-4 mx-auto my-auto text-center">
       <div className="container m-6 mx-auto flex flex-col items-center justify-center rounded-md bg-floralwhite p-4">
         <img src={AppIcon} width={30} height={30} alt="App icon" />
         <h1 className="text-2xl font-bold text-darkslategrey">My expenses</h1>
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -5,7 +5,7 @@ import Form from './Form';
 
 const Dashboard = () => {
   return (
-    <div className="container mx-auto flex h-[80vh] rounded-md bg-floralwhite p-8 text-dimgray">
+    <div className="container mx-auto flex h-[75vh] rounded-md bg-floralwhite p-8 text-dimgray">
       <div className="container flex">
         <Card title="Add expense" icon={AddExpenseIcon} />
         <Card title="Add income" icon={AddIncomeIcon} />
